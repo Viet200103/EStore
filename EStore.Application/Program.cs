d22f6f4a2a832b73a53996b0f736917651674269
VietTL <viettl200103@gmail.com>
2025-03-31 23:57:13 +0700

    Add database config

--- EStore.Application/Program.cs
+++ EStore.Application/Program.cs
@@ -1,6 +1,9 @@
 using EStore.Application.Components;
+using EStore.Application.Config;
 
 var builder = WebApplication.CreateBuilder(args);
+
+DatabaseConfigure.Configure(builder.Configuration, builder);
 
 builder.Services
     .AddRazorComponents()
